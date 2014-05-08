4d5011da63c6d6ab9c41de7745474b50d6476147
Android Chromium Automerger <chromium-automerger@android>
2014-05-08 04:36:34 +0000

    Update makefiles after merge of Chromium at 269030
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I62261f393cb6342695d5eaf4087473bd01803241

--- icuuc.host.linux-arm.mk
+++ icuuc.host.linux-arm.mk
@@ -220,6 +220,8 @@ MY_DEFS_Debug := \
 	'-DCLD_VERSION=1' \
 	'-DENABLE_PRINTING=1' \
 	'-DENABLE_MANAGED_USERS=1' \
+	'-DDATA_REDUCTION_FALLBACK_HOST="http://compress.googlezip.net:80/"' \
+	'-DSPDY_PROXY_AUTH_ORIGIN="https://proxy.googlezip.net:443/"' \
 	'-DU_COMMON_IMPLEMENTATION' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
@@ -285,6 +287,8 @@ MY_DEFS_Release := \
 	'-DCLD_VERSION=1' \
 	'-DENABLE_PRINTING=1' \
 	'-DENABLE_MANAGED_USERS=1' \
+	'-DDATA_REDUCTION_FALLBACK_HOST="http://compress.googlezip.net:80/"' \
+	'-DSPDY_PROXY_AUTH_ORIGIN="https://proxy.googlezip.net:443/"' \
 	'-DU_COMMON_IMPLEMENTATION' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
