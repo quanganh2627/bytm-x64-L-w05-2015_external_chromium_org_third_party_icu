7d67b14b2bcb941809f8c96148466c916a902d3e
Android Chromium Automerger <chromium-automerger@android>
2014-05-09 16:58:06 +0000

    Update makefiles after merge of Chromium at 269336
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I703665831b71a13a3d1f7dfb2ec6e9444f8893fc

--- icuuc.target.linux-mips.mk
+++ icuuc.target.linux-mips.mk
@@ -17,6 +17,7 @@ GYP_TARGET_DEPENDENCIES := \
 
 ### Rules for action "generate_icuuc_shim_headers":
 $(gyp_shared_intermediate_dir)/shim_headers/icuuc/target/third_party/icu/source/common/unicode/brkiter.h: gyp_local_path := $(LOCAL_PATH)
+$(gyp_shared_intermediate_dir)/shim_headers/icuuc/target/third_party/icu/source/common/unicode/brkiter.h: gyp_var_prefix := $(GYP_VAR_PREFIX)
 $(gyp_shared_intermediate_dir)/shim_headers/icuuc/target/third_party/icu/source/common/unicode/brkiter.h: gyp_intermediate_dir := $(abspath $(gyp_intermediate_dir))
 $(gyp_shared_intermediate_dir)/shim_headers/icuuc/target/third_party/icu/source/common/unicode/brkiter.h: gyp_shared_intermediate_dir := $(abspath $(gyp_shared_intermediate_dir))
 $(gyp_shared_intermediate_dir)/shim_headers/icuuc/target/third_party/icu/source/common/unicode/brkiter.h: export PATH := $(subst $(ANDROID_BUILD_PATHS),,$(PATH))
